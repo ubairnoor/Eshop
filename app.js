76724362eced97e39af2c2f04aea1305d70d7632
ubair <ubairnoor5@gmail.com>
2022-04-07 22:18:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const morgan = require('morgan');
+const mongoose = require('mongoose')
+
+const Product = require('./models/products')
+
+require('dotenv/config');
+
+//middleware 
+app.use(bodyParser.json());
+app.use(morgan('tiny'))
+
+
+const api = process.env.API_URL;
+//Routes
+const productRouter = require('./routers/products')
+//Middle Ware
+app.use(`${api}/products`,productRouter)
+
+
+//Database 
+mongoose.connect(process.env.CONNECTION_STRING,{
+    dbName:'eshop-database'
+})
+.then(()=>{
+    console.log('Database Connection is Ready')
+})
+.catch((err)=>{
+    console.log(err);
+})
+app.listen(3000,()=>{
+    console.log(api)
+    console.log('Server is running http://localhost:3000')
+})
+
---
+++ models/products.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const productSchema  = mongoose.Schema({
+    name:String,
+    image:String,
+    countInStock:{
+        type:Number,
+        required:true
+    }
+})
+
+exports.Product  = mongoose.model('Product',productSchema);
