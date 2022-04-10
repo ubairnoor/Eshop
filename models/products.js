8b8fa160bbaf9826c92ce05c7d1b18d99920fa73
ubair <ubairnoor5@gmail.com>
2022-04-10 14:49:07 +0530

    Product Schema added

--- models/products.js
+++ models/products.js
@@ -1,11 +1,61 @@
 const mongoose = require('mongoose')
 const productSchema  = mongoose.Schema({
-    name:String,
-    image:String,
-    countInStock:{
+    name:{
+        type:String,
+        required: true,
+
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    richDescription:{
+        type:String,
+        default:''
+    },
+    //Image is Array of String and type is also String
+    image:[{
+        type: String
+
+    }],
+    brand:{
         type:Number,
         required:true
+    },
+    price:{
+        type:Number,
+        default:0
+    },
+    //when we add Product we use catogory id not whole category.we say the link between the table and catorgory is id.
+    category:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true,
+        
+    },
+    countInStock:{
+        type:Number,
+        required:true,
+        min: 0,
+        max: 255
+    },
+    rating:{
+        type:Number,
+        default:0,
+    },
+    numReviews:{
+        type:Number,
+        default:0,
+    },
+    isFeatured:{
+        type:Boolean,
+        default:false,
+    },
+    dateCreated:{
+        type:Date,
+        default:Date.now,
     }
+
 })
 
 module.exports   = mongoose.model('Product',productSchema); 
