8c719c63cbce049995959d7e639baa7ef279b56f
ubair <ubairnoor5@gmail.com>
2022-04-11 20:27:21 +0530

    add Population in a table

--- routers/products.js
+++ routers/products.js
@@ -5,7 +5,7 @@ const Product = require('../models/products')
 router.get(`/`, async (req, res) => {
     //to include what data you need add in select() and if we want to execulde use  -  sign to execlude.
 
-    const productList = await Product.find().select('name image -_id');
+    const productList = await Product.find().populate('category');
     if (!productList) {
         res.status(500).json({ success: false })
     }
@@ -14,7 +14,7 @@ router.get(`/`, async (req, res) => {
 })
 //get a product by id 
 router.get(`/:id`,async(req , res)=>{
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id).populate('category');
     console.log(product)
     if(!product)
     {
