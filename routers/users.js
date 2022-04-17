6d6d1883f28b3dde8455ad018df2273d50129070
ubair <ubairnoor5@gmail.com>
2022-04-17 16:27:54 +0530

    Compare user password with Password hash

--- routers/users.js
+++ routers/users.js
@@ -2,29 +2,29 @@ const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
 const bcrypt = require("bcryptjs")
-router.get('/',async(req,res)=>{
-    const userList =  await User.find().select('-passwordHash');
-    if(!userList){ 
-        res.status(500).json({success:false})
+router.get('/', async (req, res) => {
+    const userList = await User.find()
+    if (!userList) {
+        res.status(500).json({ success: false })
     }
     res.send(userList)
 })
 //Get a single User
 
-router.get('/:id',async(req,res)=>{
-    const user=await User.findById(req.params.id).select('name email phone ');
-    if(!user){
-        res.status(500).json({message:'The user with the Given Id is not Found'})
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select('name email phone ');
+    if (!user) {
+        res.status(500).json({ message: 'The user with the Given Id is not Found' })
     }
     res.status(200).send(user)
 })
-router.post('/', async (req, res) => { 
+router.post('/', async (req, res) => {
     let user = new User({
         name: req.body.name,
         email: req.body.email,
         //we will ask a user normal Password we will not ask hashpassowrd for user.
         // internally cypt this password.
-        passwordHash:bcrypt.hashSync(req.body.password,10),
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
@@ -35,22 +35,30 @@ router.post('/', async (req, res) => {
     })
     user = await user.save();
     if (!user)
-    return res.status(400).send('the user cannot be ceated')
+        return res.status(400).send('the user cannot be ceated')
     res.send(user)
 })
 //Login 
 
-router.post('/login',async(req,res)=>{
+router.post('/login', async (req, res) => {
     console.log("Inside the Login Route")
     //login by email
-    const user = await User.findOne({email:req.body.email})
-
-    
-
-    if(!user){
+    const user = await User.findOne({ email: req.body.email })
+    console.log('>>>', user)
+    if (!user) {
         return res.status(400).send('The user not Found')
     }
-    return res.status(200).send(user)
+    console.log('Password is >>>',req.body.password)
+    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        console.log('user>>>>>>>>>>>>>>>>>>>>>' ,bcrypt.compareSync(req.body.password, user.passwordHash))
+        res.status(200).send('User Authenticated')
+    }
+    else {
+        res.status(400).send('Password is wrong')
+    }
+
+
+    // return res.status(200).send(user)
 })
 module.exports = router;
 
